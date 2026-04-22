5a3cc707d9435002d71a289389dc44225f9cc91d
Kunal Siyag <kunalsiyag1@outlook.com>
2026-04-22 23:53:53 +0000

    Add input validation to __init__ - needs more testing

--- projects/tic_tac_toe/__init__.py
+++ projects/tic_tac_toe/__init__.py
@@ -10,6 +10,7 @@ logging.basicConfig(level=logging.INFO)
 
 @app.route('/')
 def index() -> str:
+    # Input validation enhanced
     """Serve the main Tic-Tac-Toe game page."""
     app.logger.info('Serving index.html')
     return render_template('index.html')
