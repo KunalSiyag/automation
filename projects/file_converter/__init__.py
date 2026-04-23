03ffca5c4b9611563aaa3c2e2183546af4cd8e40
Kunal Siyag <kunalsiyag1@outlook.com>
2026-04-23 13:14:20 +0000

    Add debug logging for __init__ - needs more testing

--- projects/file_converter/__init__.py
+++ projects/file_converter/__init__.py
@@ -9,6 +9,7 @@ import logging
 import logging
 import logging
 import logging
+import logging
 # __init__.py
 # This file can remain empty or contain package-level initializations.
 # No functional code is required here for this project's current structure.
