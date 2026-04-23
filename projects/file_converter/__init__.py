07c62691d0ed44b81937e6c7d592b6760e473fc1
Kunal Siyag <kunalsiyag1@outlook.com>
2026-04-23 17:59:04 +0000

    Add docstrings to __init__

--- projects/file_converter/__init__.py
+++ projects/file_converter/__init__.py
@@ -1,3 +1,5 @@
 # __init__.py
 # This file can remain empty or contain package-level initializations.
 # No functional code is required here for this project's current structure.
+
+# Documentation updated - 090202
